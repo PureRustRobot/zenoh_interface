c04fcb173e0a71e5a5c4fe095c977487bb59cce3
motii <108280115+motii8128@users.noreply.github.com>
2023-12-20 12:47:55 +0900

    Update motor_controll.rs

--- src/motor_controll.rs
+++ src/motor_controll.rs
@@ -3,29 +3,29 @@ use serde::{Serialize, Deserialize};
 #[derive(Serialize, Deserialize)]
 pub struct  SingleMotor
 {
-    power:f32,
+    pub power:f32,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct  DoubleMotor
 {
-    power_0:f32,
-    power_1:f32,
+    pub power_0:f32,
+    pub power_1:f32,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct  TripleMotor
 {
-    power_0:f32,
-    power_1:f32,
-    power_2:f32,
+    pub power_0:f32,
+    pub power_1:f32,
+    pub power_2:f32,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct  QuadMotor
 {
-    power_0:f32,
-    power_1:f32,
-    power_2:f32,
-    power_3:f32
+    pub power_0:f32,
+    pub power_1:f32,
+    pub power_2:f32,
+    pub power_3:f32
 }
